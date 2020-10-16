258aa79c3cc16549dc74c9f1b2eef7b942e241f9
Dara Dermody <daradermody@gmail.com>
2020-10-16 09:50:32 +0100

    [INVE-12447] Add eslint rule to ensure dependency versions match kibi-internal (#7)

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+const memoryLeak = require('./rules/memory-leak/memory-leak');
+const sameCoreDependencyVersion = require('./rules/same-core-dependency-version/same-core-dependency-version');
+
 module.exports = {
   rules: {
-    'memory-leak': require('./rules/memory-leak/memory-leak')
+    'memory-leak': memoryLeak,
+    'same-core-dependency-version': sameCoreDependencyVersion
+  },
+  processors: {
+    '.json': {
+      preprocess: (text, filename) => {
+        if (path.basename(filename) === 'package.json') {
+          sameCoreDependencyVersion.registerPackageJsonContents(fs.readFileSync(filename, 'utf-8'));
+        }
+        return [''];
+      },
+      postprocess(messages) {
+        const flattenedMessages = [].concat(...messages)
+        return flattenedMessages.filter(error => !error.ruleId || error.ruleId === 'siren/same-core-dependency-version')
+      },
+    }
   }
 };
 
---
+++ rules/same-core-dependency-version/same-core-dependency-version.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const request = require('sync-request');
+
+let packageJsonContents;
+
+module.exports = {
+  registerPackageJsonContents: fileContents => packageJsonContents = fileContents,
+  meta: {
+    type: 'problem',
+    docs: {
+      description: 'If a dependency is also specified in the kibi-internal repo, it must have the same version here',
+      category: 'Best Practices',
+      recommended: true,
+      url: 'https://github.com/sirensolutions/eslint-plugin-siren/blob/master/rules/same-core-dependency-version/same-core-dependency-version.md'
+    },
+    schema: [] // no options
+  },
+  create: context => {
+    if (!packageJsonContents || path.basename(context.getFilename()) !== 'package.json' || !process.env.GITHUB_TOKEN) {
+      return {};
+    }
+
+    const [dependencyLinesStart, dependencyLines] = getDependencyLines(context.getSourceCode().lines);
+
+    const packageJson = JSON.parse(packageJsonContents.replace('module.exports = ', ''));
+    if (!packageJson.dependencies) {
+      return {};
+    }
+    const coreDependencies = getCoreDependencies(process.env.GITHUB_TOKEN);
+
+    for (const [dependency, version] of Object.entries(packageJson.dependencies)) {
+      if (!!coreDependencies[dependency] && coreDependencies[dependency] !== version) {
+        context.report({
+          message: `Investigate core uses ${coreDependencies[dependency]}, but this repo uses ${version} of '${dependency}'`,
+          loc: {
+            start: {
+              line: dependencyLines.findIndex(line => line.includes(`"${dependency}"`)) + dependencyLinesStart + 1,
+              column: 4
+            }
+          }
+        });
+      }
+    }
+
+    return {};
+  }
+};
+
+function getCoreDependencies(githubToken) {
+  const branch = 'master';
+  const url = `https://raw.githubusercontent.com/sirensolutions/kibi-internal/${branch}/package.json`;
+  const responseBody = request('GET', url, { headers: { Authorization: `token ${githubToken}` } }).getBody();
+  return JSON.parse(responseBody).dependencies;
+}
+
+function getDependencyLines(lines) {
+  let dependenciesBlockStart;
+  let dependenciesBlockEnd;
+  for (const [index, line] of lines.entries()) {
+    if (line.includes('"dependencies":')) {
+      dependenciesBlockStart = index;
+    }
+    if (dependenciesBlockStart && line.includes('}')) {
+      dependenciesBlockEnd = index;
+      break;
+    }
+  }
+  return [dependenciesBlockStart, lines.slice(dependenciesBlockStart, dependenciesBlockEnd + 1)];
+}
